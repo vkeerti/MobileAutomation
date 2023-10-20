f33db0229b129d333a4ba45ee09ee9b1cdad4526
Sai Vankadari <svankadari@quiktrak.com>
2023-10-20 17:47:35 +0530

    New APKFile

--- src/test/java/com/quiktrak/pages/ActionsPage.java
+++ src/test/java/com/quiktrak/pages/ActionsPage.java
@@ -10,6 +10,10 @@ import org.openqa.selenium.support.FindBy;
 import java.awt.*;
 
 public class ActionsPage extends BasePage{
+
+
+
+
     @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[3]/android.widget.Button")
     WebElement SyncBtn;
     //Test
